e84f49b71a18e0e6fd60a0ef0a73c9e086b504d7
vanitanaidu <vanita112003@gmail.com>
2017-11-12 15:57:41 -0500

    Done.

--- src/components/Pikachu.js
+++ src/components/Pikachu.js
@@ -10,6 +10,11 @@ class Pikachu extends React.Component {
     };
   }
 
+  componentDidUpdate(){
+    this.resizePikachu()
+  }
+
+
   resizePikachu = () => {
     const pikachu = document.getElementById("pikachu");
     pikachu.height = this.state.size;
